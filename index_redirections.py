22d2f3ea1049839b9331a10a93dc088f150900ac
Restyled.io <commits@restyled.io>
2023-05-02 16:04:26 +0000

    Restyled by black

--- index_redirections.py
+++ index_redirections.py
@@ -3,98 +3,90 @@ import csv
 import os
 
 
-app = Flask(__name__, template_folder=r'templates', static_folder='static')
+app = Flask(__name__, template_folder=r"templates", static_folder="static")
 
 
-@app.route('/signup', methods=['GET', 'POST'])
+@app.route("/signup", methods=["GET", "POST"])
 def sign_up():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
 
         print(username, password)
 
-        if username and password in open(r'csv\usuarios.csv').read():
-            return render_template('login.html')
+        if username and password in open(r"csv\usuarios.csv").read():
+            return render_template("login.html")
         else:
-            if username == '' or password == '':
-                return render_template('sign_up.html', error='No puede haber campos vacíos')
+            if username == "" or password == "":
+                return render_template(
+                    "sign_up.html", error="No puede haber campos vacíos"
+                )
             else:
-                with open(r'csv\usuarios.csv', 'a') as file:
+                with open(r"csv\usuarios.csv", "a") as file:
                     writer = csv.writer(file)
                     writer.writerow([username, password])
-                return redirect(url_for('login'))
+                return redirect(url_for("login"))
 
     else:
-        return render_template('sign_up.html')
+        return render_template("sign_up.html")
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
 
-        username = request.form['username']
-        password = request.form['password']
-
-        if username and password in open(r'csv\usuarios.csv').read():
-
-            return redirect(url_for('home', username=username, password=password))
+        if username and password in open(r"csv\usuarios.csv").read():
+            return redirect(url_for("home", username=username, password=password))
 
         else:
-
-            return render_template('login.html')
+            return render_template("login.html")
 
     else:
+        return render_template("login.html")
 
-        return render_template('login.html')
 
-
-@app.route('/chat', methods=['GET', 'POST'])
+@app.route("/chat", methods=["GET", "POST"])
 def chat():
-    if request.method == 'GET':
-        print('get')
+    if request.method == "GET":
+        print("get")
         global username, password
 
-        username = request.args.get('username')
-        password = request.args.get('password')
+        username = request.args.get("username")
+        password = request.args.get("password")
 
-        if username and password in open(r'csv\usuarios.csv').read():
-
-            return render_template('chat_program.html', user=username, passs=password)
+        if username and password in open(r"csv\usuarios.csv").read():
+            return render_template("chat_program.html", user=username, passs=password)
 
         else:
+            return redirect(url_for("login"))
 
-            return redirect(url_for('login'))
+    if request.method == "POST":
+        message = request.form["message"]
 
-    if request.method == 'POST':
-        message = request.form['message']
-
-        with open(r'csv\mensajes.csv', 'a') as file:
+        with open(r"csv\mensajes.csv", "a") as file:
             writer = csv.writer(file)
             writer.writerow([message])
 
-        return render_template('chat_program.html', user=username, message=message)
+        return render_template("chat_program.html", user=username, message=message)
 
 
-@app.route('/home', methods=['GET', 'POST'])
+@app.route("/home", methods=["GET", "POST"])
 def home():
-    if request.method == 'GET':
-
-        username = request.args.get('username')
-        password = request.args.get('password')
+    if request.method == "GET":
+        username = request.args.get("username")
+        password = request.args.get("password")
 
         print(username, password)
 
-        if username and password in open(r'csv\usuarios.csv').read():
-
-            return render_template('home.html', user=username, passs=password)
+        if username and password in open(r"csv\usuarios.csv").read():
+            return render_template("home.html", user=username, passs=password)
 
         else:
+            return redirect(url_for("chat", username=username, password=password))
 
-            return redirect(url_for('chat', username=username, password=password))
-
-    if request.method == 'POST':
-
+    if request.method == "POST":
         # intente emparejar un post request con un boton de html, que ahorita tengo que hacer lo de evelyn
-        if request.form['submit_button'] == 'Chat':
-            return redirect(url_for('chat', username=username, password=password))
+        if request.form["submit_button"] == "Chat":
+            return redirect(url_for("chat", username=username, password=password))
