ab3705d1fbef1eed5e5fa64118f398f0681ff9a6
Restyled.io <commits@restyled.io>
2023-05-15 03:58:41 +0000

    Restyled by autopep8

--- index_redirections.py
+++ index_redirections.py
@@ -3,20 +3,18 @@ import csv
 import os
 
 
-          
-app=Flask(__name__, template_folder=r'templates', static_folder='static')
+app = Flask(__name__, template_folder=r'templates', static_folder='static')
 
 
-
-# en el caso que no funcione a la primera, solo ingresar manuelmente la ruta 
+# en el caso que no funcione a la primera, solo ingresar manuelmente la ruta
 @app.route('/signup', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        
+
         print(username, password)
-        
+
         if username and password in open(r'csv\usuarios.csv').read():
             return render_template('login.html')
         else:
@@ -30,67 +28,49 @@ def sign_up():
 
     else:
         return render_template('sign_up.html')
-    
-
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        
+
         username = request.form['username']
         password = request.form['password']
-
 
         if username and password in open(r'csv\usuarios.csv').read():
 
             return redirect(url_for('chat', username=username, password=password))
-        
-        else:
-            
-            return render_template('login.html')
-        
-    else:
-        
-        return render_template('login.html')
 
+        else:
+
+            return render_template('login.html')
+
+    else:
+
+        return render_template('login.html')
 
 
 @app.route('/chat', methods=['GET', 'POST'])
 def chat():
     if request.method == 'POST':
-        message=request.form['message']
-        
+        message = request.form['message']
+
         return render_template('chat_program.html', message=message)
-    
+
     else:
         return render_template('chat_program.html')
-        
-        
-        
-        
+
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
         print('get')
-        
+
         username = request.args.get('username')
         password = request.args.get('password')
-        
+
         print(username, password)
 
         if username and password in open(r'csv\usuarios.csv').read():
-            
+
             return render_template('home.html', user=username, passs=password)
-        
-        
-        
-              
-
-
-
-
-
-
-
